9b629ac05dd5d82da36728f980b17e75dd9eaf2d
Bohdan <Bohdan@mail.com>
2023-06-24 11:44:23 +0300

    Add project files.

---
+++ Second_task/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Second_task
+{
+    internal class Program
+    {
+        public static bool SearchWordInString(string word,string str) 
+        {
+            bool serach = false;
+            foreach (var item in str)
+            {
+                if (item == word[0])
+                {
+                    for (int i = str.IndexOf(item),j=0; i < str.IndexOf(item)+word.Length && j<word.Length; i++,j++)
+                    {
+                        if (str[i] == word[j])
+                        {
+                            serach = true;
+                        }
+                        else
+                        {
+                            serach = false;
+                            break;
+                        }    
+                    }
+                }
+            }
+            return serach;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding=System.Text.Encoding.UTF8;
+            Console.WriteLine("Введіть рядок в якому відбуватиметься пошук:");
+            string str = Console.ReadLine();
+            Console.WriteLine("Введіть шукане слово:");
+            string word = Console.ReadLine();
+            Console.WriteLine("Результат роботи функції SearchWordInString: " + SearchWordInString(word,str));
+        }
+    }
+}
